c59a15b1e6d8a1803d9921af64dedd4c3dd77658
joeytang54 <joeytang54@gmail.com>
2022-05-14 12:35:02 -0400

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("I'm great");
+        System.out.println("I am awesome");
+
+        System.out.println("I am correct code");
     }
 }
